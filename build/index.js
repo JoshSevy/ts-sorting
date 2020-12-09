13a6eb39e4cbd41f89c6f45da47788ca32739cea
JoshSevy <joshuasevy@outlook.com>
2020-12-08 20:03:13 -0700

    Added bubble sort class and method for arrays

--- build/index.js
+++ build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('hi there');
+/**Bubble Sort algorithm*/
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([44, -2, 66, 0, 12, 99]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi there');
+/**Bubble Sort algorithm*/
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const {length} = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j+1];
+          this.collection[j+1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([44, -2, 66, 0, 12, 99]);
+sorter.sort();
+console.log(sorter.collection)
