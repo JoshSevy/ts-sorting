91d2cf0b28bdfcd0ed14b82646cd550fe026c21c
JoshSevy <joshuasevy@outlook.com>
2020-12-08 20:57:32 -0700

    Add conditional for multiple data types being passed in

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-/**Bubble Sort algorithm*/
+/**Bubble Sort algorithm class for array, string and linked-lists  */
 var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
@@ -8,10 +8,19 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                //All of this only works is collection of number[]
+                //If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                //Only going to work if collection is a string
+                //If collection is a string, run this logic instead:
+                //~~ logic to compare and swap character 
+                if (typeof this.collection === 'string') {
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-/**Bubble Sort algorithm*/
+/**Bubble Sort algorithm class for array, string and linked-lists  */
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const {length} = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = temp;
+
+        //All of this only works is collection of number[]
+        //If collection is an array of numbers
+        if(this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j+1];
+            this.collection[j+1] = temp;
+          }
+        } 
+        //Only going to work if collection is a string
+        //If collection is a string, run this logic instead:
+        //~~ logic to compare and swap character 
+        if(typeof this.collection === 'string') {
+          
         }
       }
     }
