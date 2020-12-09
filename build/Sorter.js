80c51b66e9872a50c304c6adc099254e664dd082
JoshSevy <joshuasevy@outlook.com>
2020-12-08 21:17:31 -0700

    Update sorter to use numbercollection methods to dry up code.

--- build/Sorter.js
+++ build/Sorter.js
@@ -2,16 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 var Sorter = /** @class */ (function () {
-    function Sorter(collection /**TODO: fix me!!*/) {
+    function Sorter(collection) {
         this.collection = collection;
     }
     Sorter.prototype.sort = function () {
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
                 }
             }
         }
