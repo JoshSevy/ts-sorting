5687320bc86531e0ed6843e56cb206d9ae9de04b
JoshSevy <joshuasevy@outlook.com>
2020-12-10 21:40:06 -0700

    Convert sorter class to an abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([44, -2, 66, 0, 12, 99])
-const sorter = new Sorter(numbersCollection);
+
 sorter.sort();
-console.log(numbersCollection.data);
+
 
 const charactersCollection = new CharactersCollection('XsTwDfQ');
-const charSorter = new Sorter(charactersCollection);
+
 charSorter.sort();
-console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
