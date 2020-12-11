a87d971a197c160aa9eb2e03de207af435e3fa87
JoshSevy <joshuasevy@outlook.com>
2020-12-10 20:46:17 -0700

    Add char class to sort a string passed in

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([44, -2, 66, 0, 12, 99]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('XsTwDfQ');
+var charSorter = new Sorter_1.Sorter(charactersCollection);
+charSorter.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([44, -2, 66, 0, 12, 99])
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('XsTwDfQ');
+const charSorter = new Sorter(charactersCollection);
+charSorter.sort();
+console.log(charactersCollection.data);
